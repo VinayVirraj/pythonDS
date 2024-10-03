bbfa256cc75fa75a1dc9d5b306f92769942d5255
vinaychandra1999 <vinaychandra@tamu.edu>
2024-10-03 17:34:43 -0500

    feat: debugger for rotatedSortedArray (LTCD no.33)

---
+++ binarySearch.py
@@ -0,0 +1,40 @@
+'''
+The idea is to decrease the search space for every iteration compared to linear search.
+Binary search makes the search space half for every iter making the time complexity to O(log n)
+given a sorted array
+'''
+
+
+def binarySearch(nums, target):
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = int((start+end)/2)
+
+        if target > nums[mid]: # if the target is in the second half of the array, move the start pointer
+            start = mid + 1
+        elif target < nums[mid]: # if the target is in the first half of the array, move the end pointer
+            end = mid - 1
+        else:
+            if target == nums[mid]:
+                return mid
+    return -1
+
+
+def recursiveBinarySearch(nums, target, start, end):
+    if start > end:
+        return -1
+    mid = start + (end - start) // 2
+    if nums[mid] == target:
+        return mid
+    elif target < nums[mid]:
+        return recursiveBinarySearch(nums, target, start, mid-1)
+    else:
+        return recursiveBinarySearch(nums, target, mid+1, end)
+
+
+ex_arr = [-1,0,3,5,9,12]
+
+print(f'Binary search result index: {binarySearch(ex_arr, 9)}')
+
+print(f'Recursive binary search result index: {recursiveBinarySearch(ex_arr, 9, 0, len(ex_arr))}')
---
+++ searchInRotatedSortedArray.py
@@ -0,0 +1,55 @@
+from inputs import takeArrInput
+
+'''
+when given with a sorted array (asc order) think of applying binary search. Idea is if binary search 
+algorithm is applied on the rotated sorted array, one between the two halves will be sorted
+'''
+
+def searchSortedArr(nums, target):
+    start = 0
+    end = len(nums) - 1
+    i = 0
+    while start <= end:
+        print(f"\n/////////// iter {i} ///////////")
+        print(f"Given list {nums}")
+        print(f"Given target {target}")
+        mid = start + (end - start) // 2
+        print(f"start_idx = {start}, end_idx = {end}, mid_idx = {mid}")
+        if nums[mid] ==  target:
+            print(f"final mid aka the answer {mid}\n")
+            return mid
+        elif nums[start] <= nums[mid]: # first half of the array
+            print(f"Checking if start ({nums[start]}) is less than or equal to curr mid ({nums[mid]})")
+            if nums[start] <= target < nums[mid]: # if the target is in the first half
+                print(f"Checking if the target is in between the start ({nums[start]}) and the curr mid ({nums[mid]})")
+                end = mid - 1
+                print("yes it is")
+            else:
+                print(f"Target is not between the start ({nums[start]}) and the curr mid ({nums[mid]}) so we move start pointer")
+                start = mid + 1
+        else:
+            if nums[mid] < target <= nums[end]: # if the target is in the second half
+                print(f"Checking if the target is in between the curr mid ({nums[mid]}) and end ({nums[end]})")
+                start = mid + 1
+                print("yes it is")
+            else:
+                print(f"Target is not between the curr mid ({nums[mid]}) and end ({nums[end]}) so we move end pointer")
+                end = mid - 1
+        i += 1 # just to know the iteration (not used in the function)
+    return -1
+
+
+ex_arr = [6, 7, 0, 1, 2, 3, 4, 5]
+
+print(f'\nThis is an example =====================================>>')
+searchSortedArr(ex_arr, 5)
+
+while True:
+    inp_from_user = takeArrInput()
+    if len(inp_from_user) == 1 and inp_from_user[0] == 911:
+        print("Exiting ......")
+        break
+
+    if inp_from_user:
+        value = int(input("Enter the value for which the index is needed: "))
+        print(f'\nYour Array : {inp_from_user} \nindex of {value} is : {searchSortedArr(inp_from_user, value)}\n')
