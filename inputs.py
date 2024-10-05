747ee91ca99fe53f4c07225f75832514a3bd57e4
vinaychandra1999 <vinaychandra@tamu.edu>
2024-10-04 20:38:55 -0500

    feat: changed inputs.py

--- inputs.py
+++ inputs.py
@@ -1,6 +1,6 @@
 
 def takeArrInput():
     try:
-        return list(map(int, input("Enter your list separated by spaces or type '911' to quit: ").split()))
+        return list(map(int, input("Enter your list separated by spaces or type 911 to quit: ").split()))
     except:
         print('\nEnter a valid list')
