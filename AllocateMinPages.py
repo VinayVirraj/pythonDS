91a121c77d3ee5a9c89b1a004ebe76d8a745c70b
vinayvirraj <vinayvirraj@gmail.com>
2024-10-10 12:49:18 -0500

    comments for the functions

--- AllocateMinPages.py
+++ AllocateMinPages.py
@@ -1,6 +1,10 @@
 # This function is to find the minimum among the maximum number of pages that can be allocated for a student from a given array containing pages of each book.
 
 def isvalid(m, n, arr, max_pages_allocation):
+    '''
+    This function is used to check if a mid value is valid or not. Based on this
+    left or right half of the array is chosen to reduce the time complexity to log n.
+    '''
     student_no = 1
     pages = 0
     for i in range(n):
@@ -17,8 +21,12 @@ def isvalid(m, n, arr, max_pages_allocation):
 
 
 def findPages(n ,arr ,m):
-    
-    if (m > n):
+    '''
+    arr: array containing pages of each book
+    n: number of books
+    m: number of students
+    '''
+    if (m > n):         # number of students > number of books..... not possible to solve the problem
         return -1
     start = 0
     end = sum(arr)
