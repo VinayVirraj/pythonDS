40d036148ebbba28c12a07538b901883de405297
vinaychandra1999 <vinaychandra@tamu.edu>
2024-10-10 12:38:04 -0500

    'finalcode'

--- AllocateMinPages.py
+++ AllocateMinPages.py
@@ -1,16 +1,35 @@
-# This function is to find the minimum of the maximum number of pages that can be allocated for a student from a given array of pages of each book.
+# This function is to find the minimum among the maximum number of pages that can be allocated for a student from a given array containing pages of each book.
 
-def findPages(self,n ,arr ,m):
-        start = 0
-        end = sum(arr)
-        pages = 0
-        while (start <= end):
-            mid = start + (end - start)/2
-            st = 1
-            for i in range(n):
-                # add a condition here to verify if the condition is valid to iterate through the remaining array
-                if arr[i] + pages <= mid:
-                    pages += arr[i]
-                else:
-                    st += 1
-                    pages = arr[i]
+def isvalid(m, n, arr, max_pages_allocation):
+    student_no = 1
+    pages = 0
+    for i in range(n):
+        if (arr[i] > max_pages_allocation):
+            return False
+        if (pages + arr[i] <= max_pages_allocation):
+            pages += arr[i]
+        else:
+            student_no += 1
+            pages = arr[i]
+        if (student_no > m):
+            return False
+    return True
+
+
+def findPages(n ,arr ,m):
+    
+    if (m > n):
+        return -1
+    start = 0
+    end = sum(arr)
+    answer = -1
+    while (start<=end):
+        mid = start + (end - start) // 2
+        
+        if (isvalid(m, n, arr, mid)):
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+        
+    return answer
