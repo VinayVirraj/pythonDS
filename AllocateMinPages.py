c923dd0564da18f93fc64b52f271fd7119a8bd8b
VinayVirraj <46308233+VinayVirraj@users.noreply.github.com>
2024-10-09 23:34:15 -0500

    Create AllocateMinPages.py

---
+++ AllocateMinPages.py
@@ -0,0 +1,13 @@
+def findPages(self,n ,arr ,m):
+        start = 0
+        end = sum(arr)
+        pages = 0
+        while (start <= end):
+            mid = start + (end - start)/2
+            st = 1
+            for i in range(n):
+                if arr[i] + pages <= mid:
+                    pages += arr[i]
+                else:
+                    st += 1
+                    pages = arr[i]
